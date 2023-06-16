f49153838014cc173bb9f31ab723d21cc769ee6b
sundaycrafts <sundaycrafts@users.noreply.github.com>
2023-06-16 15:46:37 +0200

    Remove Axios for edge runtime

--- src/client/HttpClient.ts
+++ src/client/HttpClient.ts
@@ -1,9 +1,83 @@
-import axios, {AxiosInstance, AxiosError, AxiosResponse} from "axios";
 import {ClientOptions, HttpClient} from "./models";
-import {ErrorHandler, PostmarkError} from "./errors/index";
+import {ErrorHandler, PostmarkError} from "./errors";
+
+type Result<D, E extends Error = Error> = { success: false, error: E } | { success: true, data: D }
+
+class WebFetch {
+    private readonly baseURL: string;
+    private readonly timeout: number;
+    private readonly validateStatus: (status: number) => boolean;
+
+    constructor({baseURL, timeout, validateStatus}: {
+        baseURL: string,
+        timeout: number,
+        responseType: "json",
+        validateStatus: (status: number) => boolean
+    }) {
+        this.baseURL = baseURL;
+        this.timeout = timeout;
+        this.validateStatus = validateStatus;
+    }
+
+    public async request<D>({method, params, url: path}: {
+        url: string,
+        method: ClientOptions.HttpMethod,
+        data: object | null,
+        headers: Headers,
+        params: Record<string, any>
+    }): Promise<Result<D, PostmarkError>> {
+        const controller = new AbortController();
+        const id = setTimeout(() => controller.abort(), this.timeout);
+
+        try {
+            const url = this.addSearchParams(new URL(path, this.baseURL), params);
+            const response = await fetch(`${url.href}`, {
+                method,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+                , signal: controller.signal
+            })
+            clearTimeout(id);
+
+            const data = await response.json();
+
+            if (!this.validateStatus(response.status)) {
+                return {
+                    success: false,
+                    error: new PostmarkError("Request failed", typeof data.ErrorCode === "number" && data.ErrorCode, response.status)
+                }
+            }
+
+            return {
+                success: true,
+                data: data.data as D
+            }
+        } catch (error) {
+            /**
+             * When abort() is called, the fetch() promise rejects with a DOMException named AbortError.
+             * @see https://developer.mozilla.org/en-US/docs/Web/API/AbortController
+             */
+            return {
+                success: false,
+                error: new PostmarkError((error as Error).message)
+            }
+        }
+    }
+
+    private addSearchParams(url: URL, params: Record<string, any>) {
+        return new URL(
+            `${url.origin}${url.pathname}?${new URLSearchParams([
+                ...Array.from(Object.entries(url.searchParams)),
+                ...Object.entries(params),
+            ])}`
+        );
+    }
+}
+
 
 export class AxiosHttpClient extends HttpClient {
-    public client!: AxiosInstance;
+    public client!: WebFetch;
     private errorHandler: ErrorHandler;
 
     public constructor(configOptions?: ClientOptions.Configuration) {
@@ -14,24 +88,19 @@ export class AxiosHttpClient extends HttpClient {
     /**
      * Create http client instance with default settings.
      *
-     * @return {AxiosInstance}
+     * @return {void}
      */
     public initHttpClient(configOptions?: ClientOptions.Configuration): void {
-        this.clientOptions = { ...HttpClient.DefaultOptions, ...configOptions };
+        this.clientOptions = {...HttpClient.DefaultOptions, ...configOptions};
 
-        const httpClient = axios.create({
+        this.client = new WebFetch({
             baseURL: this.getBaseHttpRequestURL(),
             timeout: this.getRequestTimeoutInMilliseconds(),
             responseType: "json",
-            maxContentLength: Infinity,
-            maxBodyLength: Infinity,
             validateStatus(status: number) {
                 return status >= 200 && status < 300;
             },
         });
-
-        httpClient.interceptors.response.use((response: any) => (response.data));
-        this.client = httpClient;
     }
 
     /**
@@ -41,42 +110,23 @@ export class AxiosHttpClient extends HttpClient {
      * @param path - API URL endpoint.
      * @param queryParameters - Querystring parameters used for http request.
      * @param body - Data sent with http request.
+     * @param requestHeaders
      */
-    public httpRequest<T>(method: ClientOptions.HttpMethod, path: string, queryParameters: ({} | object),
-                           body: (null | object), requestHeaders: any): Promise<T> {
+    public async httpRequest<T>(method: ClientOptions.HttpMethod, path: string, queryParameters: ({} | object),
+                                body: (null | object), requestHeaders: any): Promise<T> {
 
-        return this.client.request<void, T>({
+        const res = await this.client.request<T>({
             method,
             url: path,
             data: body,
             headers: requestHeaders,
             params: queryParameters,
-        }).catch((errorThrown: AxiosError) => {
-            return Promise.reject(this.transformError(errorThrown));
         })
-    }
 
-    /**
-     * Process callback function for HTTP request.
-     *
-     * @param error - request error that needs to be transformed to proper Postmark error.
-     *
-     * @return {PostmarkError} - formatted Postmark error
-     */
-    private transformError(errorThrown:AxiosError): PostmarkError {
-        const response: AxiosResponse | undefined = errorThrown.response;
-
-        if (response !== undefined) {
-            const status = this.adjustValue<number>(0, response.status);
-            const errorCode = this.adjustValue<number>(0, response.data.ErrorCode);
-            const message = this.adjustValue<string>(errorThrown.message, response.data.Message);
-
-            return this.errorHandler.buildError(message, errorCode, status);
-        } else if (errorThrown.message !== undefined) {
-            return this.errorHandler.buildError(errorThrown.message);
-        }
-        else {
-            return this.errorHandler.buildError(JSON.stringify(errorThrown, Object.getOwnPropertyNames(errorThrown)));
+        if (res.success) {
+            return res.data;
+        } else {
+            throw res.error;
         }
     }
 
@@ -87,9 +137,5 @@ export class AxiosHttpClient extends HttpClient {
      */
     private getRequestTimeoutInMilliseconds(): number {
         return (this.clientOptions.timeout || 60) * 1000;
-    }
-
-    private adjustValue<T>(defaultValue: T, data: T): T {
-        return (data === undefined) ? defaultValue : data;
     }
 }
